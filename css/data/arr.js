483f73f60a21e8ffd1ef29ca933eed8cc470262b
ravindra121q <112754015+ravindra121q@users.noreply.github.com>
2022-11-10 23:52:45 +0530

    day3

---
+++ css/data/arr.js
@@ -0,0 +1,53 @@
+arr1 = [
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/portrait/1920x770cd5550fd6fe64ed19f73339697501913a1ebe2567d824021883c08f193544046.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/portrait/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206274/portrait/1920x770485283887ce04c66a91f3e5dc0e20747.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/portrait/1920x77017b071bf12e24de89ab2650e35568fae.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5145092/portrait/1920x7705ce54e56eefd4b6985dc7f6e1593dffdf8a1b6bc4ede474fa16848ae0ace8d67.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x7703c6105c0047c4e98848e930ff18b4e38.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-8-3z5231124/portrait/zee5worldhitsdepth1920x770071020225904fcf2de6d48d88d128c99472a20ca.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5193948/portrait/1920x7704ba850de7a8a410cb16fa2d7468d3e77.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5210238/portrait/1920x7707684214d118049a7860032644ad058b2.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5191648/portrait/1920x77015755573326f485eb9b2ad856c296abd.jpg",
+];
+
+let arr2 = [
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5129922/portrait/1920x7707193cf3b9c41480ca9f67c210504a936.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5215371/portrait/1920x770ec63871185df4805b544c9c8c170928b0f559a2e0e6f462581b52e5a26ae4d76.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5215371/portrait/1920x770ec63871185df4805b544c9c8c170928b0f559a2e0e6f462581b52e5a26ae4d76.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5214566/portrait/1920x770310aa5c8cd9c431f8f4a81e797a6fd84.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5230618/portrait/1920x770c83518ed10864578bcc703f44431ed09.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5145156/portrait/1920x770788835a0ffcf415c8077ebaf0ef198375b371379e118446cb5e6d74740da3313.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5202609/portrait/1920x7704418ddb372c74d429e43a715c1e6a7e0.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5145103/portrait/1920x770189f07ded33c4436b6d1796409308f04.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206352/portrait/1920x770bbab5cf903824a6ba27ef2864b226e4f.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206606/portrait/1920x77072a78f9a355a4570b84ac152836481d2.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206346/portrait/1920x77072a4cb16ae5f4ed2b196533267602476.jpg",
+];
+
+arr3 = [
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5210148/portrait/1920x770b606a1ec2f5e4d569a52d66899b128aa.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5210156/portrait/1920x77093ade6adc402463c92f19a44aab06f9d.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-33204/portrait/1920x770eb95d00347474d48a4307809cedc50a0.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-369273/portrait/1920x7706dbcd377e0ba4e40b8b4533066e64b791b570aed8b7047a6b8593ffac10e0ce9.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-101-10z5226524/portrait/1920x770e578d29207064883898fd35bbf51977d.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-movie_1511761297/portrait/1920x770580834ad70b34512ba4250d277e68313.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51560/portrait/1920x770087ff5f6d15f44f98b6a27adc7130aac.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51560/portrait/1920x770087ff5f6d15f44f98b6a27adc7130aac.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51198/portrait/1920x770a02410f0ba6b487cb2b2309deaff5751.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-27132/portrait/1920x770d1edc80582d845ffadbb89aa802c967b5ee1a2f6dd514d9bb193c3c269165cc3.jpg",
+];
+arr4 = [
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-1298/portrait/1920x770d9ead07e5b304a989d85cf8c6e0113fb.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5145215/portrait/1920x770abf42130d9cf41dab2bf9e648cf3193b4100d873cc6547a8a4a4c92739160b3f.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-3444/portrait/1920x77000fb0aef19c44cba86897d8612b2758cbbab3c59cedd49a4b6d9a0f01eab56b5.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-2356/portrait/1920x770ffdfc616c75840cf95545cd67e3476d6e2fb2ce4623c4ebaa29b36ba97edf983.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-3348/portrait/1920x770385568547.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-3351/portrait/1920x7702003856115.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z597039/portrait/1920x7708ac7dc91daf34fd390f8c9042bf8ec42.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-1741/portrait/061741incover.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-1102/portrait/1920x77011433436151143343615545fc524f6d34a908f70d2cbd4911374.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-3051/portrait/1920x770in1063538472.jpg",
+  "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-1386/portrait/1920x770958311632.jpg",
+];
---
+++ newdata.js
@@ -0,0 +1,51 @@
+let b5 = document.querySelector(".cat5>.carousel-cell:nth-child(1)");
+b5.style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/portrait/1920x770cd5550fd6fe64ed19f73339697501913a1ebe2567d824021883c08f193544046.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(2)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/portrait/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(3)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206274/portrait/1920x770485283887ce04c66a91f3e5dc0e20747.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(4)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/portrait/1920x77017b071bf12e24de89ab2650e35568fae.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(5)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5145092/portrait/1920x7705ce54e56eefd4b6985dc7f6e1593dffdf8a1b6bc4ede474fa16848ae0ace8d67.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(6)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x7703c6105c0047c4e98848e930ff18b4e38.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(7)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-8-3z5231124/portrait/zee5worldhitsdepth1920x770071020225904fcf2de6d48d88d128c99472a20ca.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(8)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg')";
+
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(9)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5193948/portrait/1920x7704ba850de7a8a410cb16fa2d7468d3e77.jpg')";
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(10)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5210238/portrait/1920x7707684214d118049a7860032644ad058b2.jpg')";
+document.querySelector(
+  ".cat5>.carousel-cell:nth-child(11)"
+).style.backgroundImage =
+  "url('https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5191648/portrait/1920x77015755573326f485eb9b2ad856c296abd.jpg')";
